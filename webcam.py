4c87725940c711ad57b7dc9ade1ef4b13179dce1
Charles <105826694+FreeCharles02@users.noreply.github.com>
2025-09-01 10:31:46 -0500

    Model predict

--- webcam.py
+++ webcam.py
@@ -1,68 +1,23 @@
-import cv2, time, pandas
-from datetime import datetime
-static_back = None
-motion_list = [None, None]
-time=[]
-df=pandas.DataFrame(columns = ["Start", "End"])
-video = cv2.VideoCapture(0)
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('yolo11n.pt')
+webcamera = cv2.VideoCapture(0)
+
 
 while True:
-    check, frame = video.read()
-    motion = 0
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21,21), 0)
-
-    if static_back is None:
-        static_back = gray 
-        continue
-
-    diff_frame = cv2.absdiff(static_back, gray)
-
-    thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
-    thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
-
-    cnts,_ = cv2.findContours(thresh_frame.copy(), 
-                        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
-
-    for contour in cnts: 
-        if cv2.contourArea(contour) < 10000:
-            continue
-        motion = 1
-
-        (x, y, w, h) = cv2.boundingRect(contour)
-
-        cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 3)
-        
-    motion_list.append(motion)
-
-    motion_list = motion_list[-2:]
-
-    if motion_list[-1] == 1 and motion_list[-2] == 0:
-        time.append(datetime.now())
-
-    if motion_list[-1] == 0 and motion_list[-2] == 1:
-        time.append(datetime.now())
-
-    cv2.imshow("Gray Frame", gray)
-
-    cv2.imshow("Difference Frame", diff_frame)
-
-    cv2.imshow("Threshold Frame", thresh_frame)
-
-    cv2.imshow("Color Frame", frame)
-
-    key = cv2.waitKey(1)
-
-    if key == ord('q'):
+    success, frame = webcamera.read()
+    frame = cv2.flip(frame, 1)
     
-        if motion == 1:
-            time.append(datetime.now())
-        break
+    results = model.predict(frame)
+   # results = model.predict("/home/bigboss/Pictures/monitor.png", save=True, imgsz=320, conf=0.5)
+    
+    cv2.putText(frame, f"Total: {len(results[0].boxes)}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0, 255), 2, cv2.LINE_AA)
+    cv2.imshow("Live Camera", results[0].plot())
+   
 
-for i in range(0, len(time), 2): 
-    df = df.append({"Start": time[i], "End":time[i + 1]}, ignore_index = True)
-df.to_csv("Time_of_movements.csv")
+    if cv2.waitKey(1) == ord('q'):
+         break
 
-video.release()
-
+webcamera.release()
 cv2.destroyAllWindows()
